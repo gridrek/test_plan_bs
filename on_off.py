2cbe57adc00de05a0b10e334fe2c6fb8cce540f2
gridrek <gabinus_93@hotmail.com>
2024-05-27 14:31:29 +0200

    adding scheduler

--- on_off.py
+++ on_off.py
@@ -7,8 +7,18 @@ class LampSignal():
     #Sending fake signal
     radnom_number = random.random()
     if (radnom_number > 0.1):
+      print("signal sent")
       return True
+    print("signal failed to send")
     return False
+
+class ScheduleSignal():
+  lamp_signals: list[LampSignal]
+
+  def scheduleOn():
+    pass
+  def scheduleOff():
+    pass
 
 lamp_signal: LampSignal = LampSignal()
 
