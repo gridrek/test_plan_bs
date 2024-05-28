adf165e8f8b8fdbf05778e4f7872fa93b5c6966f
gridrek <gabinus_93@hotmail.com>
2024-05-28 11:22:46 +0200

    more testing

--- test_on_off.py
+++ test_on_off.py
@@ -1,5 +1,5 @@
 from on_off import on, off, SignalScheduler, LampSignal
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Tuple
 
 def test_on():
@@ -12,12 +12,20 @@ def test_off():
 
 def test_schedule_on():
   scheduler = SignalScheduler()
+  before_schedule_time = datetime.now()
 
   scheduler.scheduleOnSignal(10)
+  after_schedule_time = datetime.now()
+
   assert len(scheduler.lamp_signals) == 1
   assert isinstance(scheduler.lamp_signals[0], tuple)
   assert isinstance(scheduler.lamp_signals[0][0], LampSignal)
   assert isinstance(scheduler.lamp_signals[0][1], datetime)
+  scheduled_time = scheduler.lamp_signals[0][1]
+  lower_bound = before_schedule_time + timedelta(seconds=10)
+  upper_bound = after_schedule_time + timedelta(seconds=10)
+  assert lower_bound <= scheduled_time <= upper_bound
+
 
 #test_on()
 test_schedule_on()
