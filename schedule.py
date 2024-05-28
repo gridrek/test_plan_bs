45977a47a8ba4795ef9ddb788ffc140f440c0606
gridrek <gabinus_93@hotmail.com>
2024-05-28 18:45:28 +0200

    formatting and stuff

--- schedule.py
+++ schedule.py
@@ -3,37 +3,30 @@ from lampSignal import LampSignal
 from typing import List, Tuple
 
 
+class LampSignal:
+    def __init__(self, state: bool) -> None:
+        self.state = state
+
+
 class SignalScheduler:
-	def __init__(self) -> None:
-		self.lamp_signals: List[Tuple[LampSignal, datetime]] = []
+    def __init__(self) -> None:
+        self.lamp_signals: List[Tuple[LampSignal, datetime]] = []
 
-	def _schedule_signal(self, time_in_seconds: int, state: bool):
-		signal = LampSignal(state)
-		scheduled_time = datetime.now() + timedelta(seconds=time_in_seconds)
-		new_entry = (signal, scheduled_time)
-				
-		for index, (_, scheduled_time_existing) in enumerate(self.lamp_signals):
-			if new_entry[1] < scheduled_time_existing:
-				self.lamp_signals.insert(index, new_entry)
-				return
-				
-		self.lamp_signals.append(new_entry)
+    def _schedule_signal(self, time_in_seconds: int, state: bool):
+        signal = LampSignal(state)
+        scheduled_time = datetime.now() + timedelta(seconds=time_in_seconds)
+        new_entry = (signal, scheduled_time)
 
-	def scheduleOnSignal(self, time_in_seconds: int):
-		self._schedule_signal(time_in_seconds, True)
+        for index, (_, scheduled_time_existing) in enumerate(self.lamp_signals):
+            if new_entry[1] < scheduled_time_existing:
+                self.lamp_signals.insert(index, new_entry)
+                return new_entry
 
-	def scheduleOffSignal(self, time_in_seconds: int):
-		self._schedule_signal(time_in_seconds, False)
+        self.lamp_signals.append(new_entry)
+        return new_entry
 
+    def scheduleOnSignal(self, time_in_seconds: int):
+        return self._schedule_signal(time_in_seconds, True)
 
-signalScheduler = SignalScheduler()
-print()
-print(f"NOW: {datetime.now()}")
-signalScheduler.scheduleOnSignal(20)
-signalScheduler.scheduleOnSignal(10)
-signalScheduler.scheduleOnSignal(30)
-signalScheduler.scheduleOnSignal(5)
-signalScheduler.scheduleOffSignal(15)
-
-for signal, scheduled_time in signalScheduler.lamp_signals:
-		print(f"LampSignal(status={'on' if signal.status else 'off'}) at {scheduled_time.strftime('%Y-%m-%d %H:%M:%S')}")
+    def scheduleOffSignal(self, time_in_seconds: int):
+        return self._schedule_signal(time_in_seconds, False)
--- test_on_off.py
+++ test_on_off.py
@@ -1,12 +1,14 @@
 from on_off import on, off
 
+
 def test_on():
-  result = on()
-  assert result == True
+    result = on()
+    assert result == True
+
 
 def test_off():
-  result = off()
-  assert result == True
+    result = off()
+    assert result == True
 
 
 test_on()
